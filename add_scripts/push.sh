12eb57ac29df403733fc144bc66ab1f8b03d31b8
brooswit <brooswit@OldBoi.local>
2018-12-30 20:24:01 -0800

    squash! Automated Development Deployment

--- add_scripts/push.sh
+++ add_scripts/push.sh
@@ -1,5 +1,5 @@
 node ./add_scripts/common/incrementVersion
 git add .
 git commit -m 'squash! Automated Development Deployment'
-git pull --rebase
+git pull origin HEAD --rebase
 git push origin HEAD
