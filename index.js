eb1fa1baa46a1ecb17d8e6eb2dc5c3877deb036b
brooswit <brooswit@OldBoi.local>
2018-12-30 19:46:53 -0800

    squash! Automated Development Deployment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const const Cthulhu = require('Cthulhu')
+const Nunur = require('./src/nunur')
+const nunur = new Nunur()
+
+const Cthulhu = require('Cthulhu')
 const cthulhu = new Cthulhu()
 cthulhu.start()
