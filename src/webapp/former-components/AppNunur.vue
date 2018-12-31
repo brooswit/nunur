2c9b24da4ab4259f69e4ca9da0c5e902d134e825
Jennifer <jen.dxon@gmail.com>
2018-12-30 20:21:14 -0800

    Created new directory for original vue components.

---
+++ src/webapp/former-components/AppNunur.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="nunur">
+    <link rel="stylesheet" type="text/css" :href="$store.getters.currentUserView.themeUrl" />
+    <b-container fluid class="p-0">
+      <b-row >
+        <b-col>
+          <topRow/>
+        </b-col>
+      </b-row>
+      <b-row >
+        <b-col>
+          <isInitializedWrapper>
+            <router-view></router-view>
+          </isInitializedWrapper>
+        </b-col>
+      </b-row>
+      <b-row  align-v="end">
+        <b-col>
+          <bottomRow/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNunur',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ src/webapp/former-components/WrapperIsInitialized.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-container fluid class="p-0">
+        <b-row >
+            <b-col>
+                <template v-if="!$store.state.isInitialized">
+                    Initializing...
+                </template>
+                <template v-else>
+                    <slot></slot>
+                </template>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+  name: 'WrapperIsInitialized'
+}
+</script>
+
+<style>
+
+</style>
