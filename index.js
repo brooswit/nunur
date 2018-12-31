73dbde1fcecfdc89bd39d2460a75847c5f2be96c
brooswit <brooswit@OldBoi.local>
2018-12-30 21:57:03 -0800

    squash! Automated Development Deployment

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Nunur = require('./src/nunur')
 const nunur = new Nunur()
 
 nunur.express.use(express.static(path.join(__dirname, 'build')))
-.use(express.static(path.join(__dirname, 'public')))
+nunur.express.use(express.static(path.join(__dirname, 'public')))
 
 nunur.start()
