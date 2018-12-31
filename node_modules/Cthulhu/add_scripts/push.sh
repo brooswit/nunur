e596560963078c4cfb47a16441c008c2a976ff98
brooswit <brooswit@OldBoi.local>
2018-12-30 19:24:46 -0800

    squash! Automated Development Deployment

--- node_modules/Cthulhu/add_scripts/push.sh
+++ node_modules/Cthulhu/add_scripts/push.sh
@@ -1,4 +1,4 @@
 node ./add_scripts/common/incrementVersion
 git add .
-git commit -m 'AUTOMATED DEVELOPMENT DEPLOYMENT'
+git commit -m 'squash! Automated Development Deployment'
 git push origin HEAD
