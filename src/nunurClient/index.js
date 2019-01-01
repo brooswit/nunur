877c507e573a24d5d995d06be7915cb13f5be6df
brooswit <brooswit@OldBoi.local>
2018-12-31 21:28:02 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -1,11 +1,11 @@
 const {Minion} = require('Cthulhu')
-module.exports = class NunurClient {
+module.exports = class NunurClient extends process{
   constructor(username, password) {
     this._minion = new Minion()
     this._minion.start()
     this._minion.requestTask('login', {username, password}, (success) => {
       if (success) {
-        
+
       }
     })
   }
