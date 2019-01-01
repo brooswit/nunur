94e4334d2af59eee810b4b70f9c91067d9740099
brooswit <brooswit@OldBoi.local>
2018-12-31 23:16:46 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -19,7 +19,6 @@ module.exports = class NunurClient extends Process {
   async message(target, message) {
     const authRes = await this.promiseToAuthenticate
     const authSuccess = authRes['success']
-    const
     if(!authSuccess) return false
 
     const token = authSuccess['token']
