9ba335b80379da88fb548129cca54c2368f1f937
brooswit <brooswit@OldBoi.local>
2018-12-31 22:40:53 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -13,6 +13,6 @@ module.exports = class NunurClient extends Process {
   async message(identity, message) {
     if(!await promiseToEmit(this, 'authenticate')) return false
     const {token} = this
-    this._minion.requestTask('message', {token, identity, message})
+    this._minion.requestTask('message', {token, identity, message}, )
   }
 }
