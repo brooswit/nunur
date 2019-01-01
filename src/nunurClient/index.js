16afcfc834c5d5cd4ea57f2b93767862a0254f20
brooswit <brooswit@OldBoi.local>
2019-01-01 01:17:20 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -19,6 +19,8 @@ module.exports = class NunurClient extends Process {
       this._minion.hookEvent(`message/${identity}`,(message)=>{
         this.emit('receive', message)
       })
+
+      await this.promise
     })
   }
 
