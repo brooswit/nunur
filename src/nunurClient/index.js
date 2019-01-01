7ff64a1b79985cf1dc7a096d626aef01ad3f4002
brooswit <brooswit@OldBoi.local>
2018-12-31 23:36:41 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -2,7 +2,7 @@ const {Process} = require('brooswit-common')
 const {Minion} = require('Cthulhu')
 module.exports = class NunurClient extends Process {
   constructor(url, identity, password) {
-    super(async () => {
+    super(async (process) => {
       this.promiseToAuthenticate = promiseToEmit(this, 'authenticate')
 
       this._minion = new Minion(url)
