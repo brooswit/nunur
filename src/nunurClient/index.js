e25f48bdb4fe599c89a896cbace8c2e96be7c661
brooswit <brooswit@OldBoi.local>
2018-12-31 21:25:41 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -3,8 +3,8 @@ module.exports = class NunurClient {
   constructor(username, password) {
     this._minion = new Minion()
     this._minion.start()
-    this._minion.requestTask('login', {username, password}, (response) => {
-      if (response)
+    this._minion.requestTask('login', {username, password}, (loginValid) => {
+      if (loginValid)
     })
   }
 
