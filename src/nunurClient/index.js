9172cedd79f0476418912adaa1088cb956f3b138
brooswit <brooswit@OldBoi.local>
2019-01-01 01:12:40 -0800

    squash! Automated Development Deployment

--- src/nunurClient/index.js
+++ src/nunurClient/index.js
@@ -16,7 +16,7 @@ module.exports = class NunurClient extends Process {
       this.emit('authenticate', authRes)
       if(!authRes.success) return
 
-      this.hookEvent(`message/${identity}`,(message)=>{
+      this._minion.hookEvent(`message/${identity}`,(message)=>{
         this.emit('receive', message)
       })
     })
