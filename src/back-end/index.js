244816552d31ea524ec16f4e5cac8d9da09c75bb
brooswit <brooswit@OldBoi.local>
2019-06-30 08:25:50 -0700

    add tokens to backend login

--- src/back-end/index.js
+++ src/back-end/index.js
@@ -12,59 +12,100 @@ const expressApp = express()
 expressApp.use(bodyParser.json())
 expressApp.use(express.static('./build/public'))
 
-let users = []
-
 let eventEmitter = new EventEmitter()
 // setup ws
 enableWs(expressApp)
 
-async function authenticate(identifier , authentication) {
-  let user = await fetchUser(identifier) || await newUser(authentication)
-  let valid = await verify(user, authentication)
-  if(valid) await saveUser(identifier, user)
-  return valid ? user : null
-  
+async function authenticate(identifier , authentication, tokenId = null) {
+  let session = await fetchUser(identifier) || await newUser(identifier, authentication)
+  session.token = tokenId ? await fetchToken(identifier, tokenId) : null
+  if (!await verify(session, authentication)) return null
+  session.token |= await newToken(session)
+  await saveSession(session)
+  return session
+
   async function fetchUser(identifier) {
     console.warn('fetch user')
     return await new Promise(async(resolve) => {
       redis.hget('user', identifier, async (err, userJson) => {
-        if (err) {
-          throw err
-        }
+        if (err) throw err
         let userData = safeJSON.parse(userJson, null)
-        if(userData && !userData.hash) userData = null;
+        if(userData) {
+          userData.identifier = identifier
+          if(!userData.hash) userData = null;
+        }
         resolve(userData)
       })
     })
   }
 
-  async function newUser(authentication) {
+  async function newUser(identifier, authentication) {
     console.warn('new user')
     return await new Promise(async(resolve) => {
       crypto.randomBytes(16, async (err, salt) => {
-        if (err) {
-          throw err
-        }
+        if (err) throw err
         let hash = await argon2i.hash(authentication, salt)
-        resolve({hash})
+        resolve({identifier, hash})
       })
     })
   }
 
-  async function verify(user, authentication) {
-    console.warn('verify: ' + user.hash)
-    return await argon2i.verify(user.hash, authentication)
+  async function verify(session, authentication) {
+    return await argon2i.verify(session.token ? session.token.hash : token.hash, authentication)
   }
 
-  async function saveUser(identifier, user) {
-    console.warn('save user')
-    await new Promise((resolve) => {
-      let userJson = JSON.stringify(user)
-      redis.hset('user', identifier, userJson, async (err) => {
-        if (err) {
-          throw err
+  async function saveSession(session) {
+    console.warn('save session')
+    await Promise.all([
+      new Promise((resolve) => {
+        let userJson = JSON.stringify({
+          hash: session.hash
+        })
+        redis.hset('user', session.identifier, userJson, async (err) => {
+          if (err) throw err
+          resolve()
+        })
+      }),
+      new Promise((resolve) => {
+        let tokenJson = JSON.stringify({ 
+          userIdentifier: session.identifier,
+          hash: session.token.hash
+        })
+        redis.hset('token', session.id, tokenJson, async (err) => {
+          if (err) throw err
+          resolve()
+        })
+      }),
+    ])
+  }
+
+  async function fetchToken(userIdentifier, tokenId) {
+    return await new Promise(async(resolve) => {
+      redis.hget('token', tokenId, async (err, tokenJson) => {
+        if (err) throw err
+        let tokenData = safeJSON.parse(tokenJson, null)
+        if(tokenData) {
+          tokenData.id = tokenId
+          if (!tokenData.hash
+            || !tokenData.userIdentifier
+            || tokenData.userIdentifier != userIdentifier
+          ) {
+            userData = null
+          }
         }
-        resolve()
+
+        resolve(tokenData)
+      })
+    })
+  }
+
+  async function newToken(userIdentifier) {
+    return await new Promise((resolve) => {
+      crypto.randomBytes(16, async (err, secret) => {
+        if (err) throw err
+        
+        let hash = await argon2i.hash(authentication, secret)
+        resolve({userIdentifier, secret, hash})
       })
     })
   }
